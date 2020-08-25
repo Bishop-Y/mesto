0ae5f569abfabe016960dc84000eb84efb1bed54
Artem Epifanov <dizzydw@yandex.ru>
2020-08-25 03:16:02 +0300

    сделал первое ревью

--- scripts/index.js
+++ scripts/index.js
@@ -24,42 +24,19 @@ const popupViewingImage = document.querySelector(`.popup__photo`);
 const popupViewingImageTitle = document.querySelector(`.popup__photo-title`);
 const closeButtonPopupViewing = document.querySelector(`.popup__close-button_viewing`);
 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+function switchClassPopup(popup) {
+    popup.classList.toggle(`popup_opened`);
+}
 
-function popupProfile_opened() {
-    popupProfile.classList.add(`popup_opened`);
+function openPopupProfile() {
+    switchClassPopup(popupProfile)
 
     namePopup.value = nameProfile.textContent;
     jobPopup.value = jobProfile.textContent;
 }
 
-function popupProfile_closed() {
-    popupProfile.classList.remove(`popup_opened`);
+function closePopupProfile() {
+    switchClassPopup(popupProfile);
 }
 
 function popupProfileSubmitHandler(evt) {
@@ -68,65 +45,67 @@ function popupProfileSubmitHandler(evt) {
     nameProfile.textContent = namePopup.value;
     jobProfile.textContent = jobPopup.value;
 
-    popupProfile_closed();
+    closePopupProfile();
 }
 
-function popupCard_opened() {
-    popupCard.classList.add(`popup_opened`);
+function openPopupCard() {
+    switchClassPopup(popupCard);
 }
 
-function popupCard_closed() {
-    popupCard.classList.remove(`popup_opened`);
+function closePopupCard() {
+    switchClassPopup(popupCard);
 }
 
 function popupCardSumbitHandler(evt) {
     evt.preventDefault();
 
-    const photoCardC = createPhotoCard(placePopup.value, linkPopup.value);
-    photoGridCards.prepend(photoCardC);
+    const photoCard = createPhotoCard(placePopup.value, linkPopup.value);
+    photoGridCards.prepend(photoCard);
 
-    popupCard_closed();
+    closePopupCard();
 }
 
 function deletePhotoCard(evt) {
     evt.target.closest(`.photo-grid__container`).remove();
 }
 
-function popupViewing_closed() {
-    popupViewing.classList.remove(`popup_opened`);
+function closePopupViewing() {
+    switchClassPopup(popupViewing);
 }
 
 function createPhotoCard(name, link) {
-    const photoCardR = photoGridTemplate.cloneNode(true);
-    const cardImage = photoCardR.querySelector(`.photo-grid__image`);
+    const cardTemplate = photoGridTemplate.cloneNode(true);
+    const cardImage = cardTemplate.querySelector(`.photo-grid__image`);
 
     cardImage.src = link;
     cardImage.alt = `${name}`;
-    photoCardR.querySelector(`.photo-grid__name`).textContent = name;
-    photoCardR.querySelector(`.photo-grid__like-button`).addEventListener(`click`, function(evt) {
+    cardTemplate.querySelector(`.photo-grid__name`).textContent = name;
+    cardTemplate.querySelector(`.photo-grid__like-button`).addEventListener(`click`, function(evt) {
         evt.target.classList.toggle(`photo-grid__like-button_activated`);
     });
-    photoCardR.querySelector(`.photo-grid__delete-button`).addEventListener(`click`, deletePhotoCard);
+    cardTemplate.querySelector(`.photo-grid__delete-button`).addEventListener(`click`, deletePhotoCard);
     cardImage.addEventListener(`click`, function(){
-        popupViewing.classList.add(`popup_opened`);
+        switchClassPopup(popupViewing);
         popupViewingImage.src = cardImage.src;
         popupViewingImage.alt = cardImage.alt;
         popupViewingImageTitle.textContent = name;
     });
-    return photoCardR;
+    return cardTemplate;
 }
 
-initialCards.forEach(function (element) {
+function renderCard(element) {
     const photoCard = createPhotoCard(element.name, element.link);
     photoGridCards.append(photoCard);
-})
+}
 
-editButtonProfile.addEventListener(`click`, popupProfile_opened);
-closeButtonPopupProfile.addEventListener(`click`, popupProfile_closed);
+initialCards.forEach(renderCard);
+
+editButtonProfile.addEventListener(`click`, openPopupProfile);
+closeButtonPopupProfile.addEventListener(`click`, closePopupProfile);
 formElementProfile.addEventListener(`submit`, popupProfileSubmitHandler);
 
-addButtonPhotoCard.addEventListener(`click`, popupCard_opened);
-closeButtonPopupCard.addEventListener(`click`, popupCard_closed);
+addButtonPhotoCard.addEventListener(`click`, openPopupCard);
+closeButtonPopupCard.addEventListener(`click`, closePopupCard);
 formElementCard.addEventListener(`submit`, popupCardSumbitHandler);
 
-closeButtonPopupViewing.addEventListener(`click`, popupViewing_closed);
+closeButtonPopupViewing.addEventListener(`click`, closePopupViewing);
